3509bf8a06800233bac2c4c942e76653fc904ee9
James <jholder@clearwaterpay.com>
2020-07-08 23:35:28 -0500

    Adding functionality for user management

---
+++ AuthManager/CashData.Library/Internal/DataAccess/SQLDataAccess.cs
@@ -0,0 +1,39 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CashData.Library.Internal.DataAccess
+{
+    internal class SQLDataAccess
+    {
+        public string  GetConnectionString(string name)
+        {
+            return ConfigurationManager.ConnectionStrings[name].ConnectionString;
+        }
+        public List<T> LoadData<T, U>(string storedProcedure, U parameters, string connectionStringName)
+        {
+            string connectionString = GetConnectionString(connectionStringName);
+            using(IDbConnection cnn = new SqlConnection(connectionString))
+            {
+                List<T> rows = cnn.Query<T>(storedProcedure, parameters,
+                    commandType: CommandType.StoredProcedure).ToList();
+                return rows;
+            }
+        }
+        public void LoadData<T>(string storedProcedure, T parameters, string connectionStringName)
+        {
+            string connectionString = GetConnectionString(connectionStringName);
+            using(IDbConnection cnn = new SqlConnection(connectionString))
+            {
+                cnn.Execute(storedProcedure, parameters,
+                    commandType: CommandType.StoredProcedure);
+            }
+        }
+    }
+}
